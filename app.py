a63a70016b528055ea72f2fcdbb2ef735e32e55e
arthurarty <arthur.nangai@gmail.com>
2018-11-14 21:51:53 +0300

    [bg] Change input to int.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import random
 def random_gen():
     rand_num = random.randint(0, 10)
     for turn in range(5):
-        user_guess = input('Please guess your magic number (0-10): ')
+        user_guess = int(input('Please guess your magic number (0-10): '))
         if user_guess < rand_num:
             print ('Guess is too low. Try again')
         elif user_guess > rand_num:
@@ -13,5 +13,5 @@ def random_gen():
             print ('Game Over')
             break
         turn = turn + 1    
-                       
+               
 random_gen()
